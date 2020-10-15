5f6424767a8cd7e036c4765757ab6dc4278ac2ec
Mateo Marchisone <mateo.marchisone@gmail.com>
2020-10-14 21:28:18 -0300

    Apertura de ventana principal

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print("hola")
-print("Soy Lolo")
-print("Soy Mateo")
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import Slot
+
+from Propiedad_ui import Ui_Propiedad
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Propiedad()
+        self.ui.setupUi(self)
+
+    @Slot()
+    def casa_slot(self):
+        pass
+
+    @Slot()
+    def departamento_slot(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())

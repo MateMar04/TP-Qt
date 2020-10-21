6b2234eb09a4a8bb5ef5eeead287e917bcc3741a
Lorenzo <lorenzo.galaverna1@gmail.com>
2020-10-21 12:08:20 -0300

    pb_borrar_todo

--- main.py
+++ main.py
@@ -37,6 +37,27 @@ class MainWindow(QMainWindow):
         self.toggle_inputs(self.all, True)
         self.toggle_inputs([self.ui.le_codigo_postal, self.ui.le_precio_ars], False)
 
+    @Slot()
+    def borrar_todo_slot(self):
+        self.ui.le_direccion.setText("")
+        self.ui.le_codigo_postal.setText("")
+        self.ui.le_metros_cuadrados.setText("")
+        self.ui.le_cant_pisos.setText("")
+        self.ui.le_nro_piso.setText("")
+        self.ui.le_nro_departamento.setText("")
+        self.ui.le_nro_ambientes.setText("")
+        self.ui.le_nro_dormitorios.setText("")
+        self.ui.le_nro_banios.setText("")
+        self.ui.cb_amueblado.setCurrentIndex(0)
+        self.ui.cb_habitado.setCurrentIndex(0)
+        self.ui.le_precio_ars.setText("")
+        self.ui.le_precio_uds.setText("")
+        self.ui.le_imagenes.setText("")
+
+    @Slot()
+    def registrar_slot(self):
+        pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

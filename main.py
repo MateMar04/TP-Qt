b4cc99423b06d46cb7fc25dcc53751a35386c064
Lorenzo <lorenzo.galaverna1@gmail.com>
2020-11-10 19:51:48 -0300

    Agregue main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import requests
-import pickle
 
 from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PySide2.QtCore import Slot
@@ -25,7 +24,8 @@ class MainWindow(QMainWindow):
                     self.ui.le_nro_dormitorios, self.ui.le_nro_banios, self.ui.cb_amueblado, self.ui.cb_habitado,
                     self.ui.le_precio_ars, self.ui.le_precio_uds, self.ui.le_imagenes, self.ui.pb_registrar]
 
-        self.propiedades = []
+        self.departamentos = []
+        self.casas = []
 
         self.toggle_inputs(self.all, False)
 
@@ -68,10 +68,6 @@ class MainWindow(QMainWindow):
         self.toggle_inputs([self.ui.le_cant_pisos], False)
 
     @Slot()
-    def lista_slot(self):
-        self.list_window.show()
-
-    @Slot()
     def registrar_slot(self):
         departamento = Departamento(
             self.ui.le_direccion.text(),
@@ -103,13 +99,11 @@ class MainWindow(QMainWindow):
         )
 
         if not departamento.is_empty_f():
-            self.propiedades.append(departamento)
-            self.save_file()
+            self.departamentos.append(departamento)
             self.borrar_todo_slot()
 
             if not casa.is_empty_h():
-                self.propiedades.append(casa)
-                self.save_file()
+                self.casas.append(casa)
                 self.borrar_todo_slot()
 
             if self.list_window.isHidden():
@@ -134,20 +128,6 @@ class MainWindow(QMainWindow):
         self.ui.le_precio_ars.setText("")
         self.ui.le_precio_uds.setText("")
         self.ui.le_imagenes.setText("")
-
-    def save_file(self):
-        with open("propiedades.v", "wb") as file:
-            pickle.dump(self.propiedades, file)
-
-    def read_file(self):
-        try:
-            with open("propiedades.v", "rb") as file:
-                propiedades = pickle.load(file)
-        except FileNotFoundError:
-            propiedades = []
-        return propiedades
-
-
 
 
 if __name__ == "__main__":

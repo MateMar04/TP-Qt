1add2ed1350fa6111a2da9fd9d9a17781646a354
Lorenzo <lorenzo.galaverna1@gmail.com>
2020-11-11 11:26:55 -0300

    puse los rows

--- ventana_lista.py
+++ ventana_lista.py
@@ -1,3 +1,4 @@
+from PySide2 import QtWidgets
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtCore import Slot
 
@@ -14,4 +15,31 @@ class ListWindow(QMainWindow):
     def volver_slot(self):
         self.window().close()
 
+    def agregar_row_casa(self, casa):
+        row_position = self.ui.tb_casas.rowCount()
+        self.ui.tb_casas.insertRow(row_position)
+        self.ui.tb_casas.setItem(row_position, 0, QtWidgets.QTableWidgetItem(casa.direccion))
+        self.ui.tb_casas.setItem(row_position, 1, QtWidgets.QTableWidgetItem(casa.cant_pisos))
+        self.ui.tb_casas.setItem(row_position, 2, QtWidgets.QTableWidgetItem(casa.codigo_postal))
+        self.ui.tb_casas.setItem(row_position, 3, QtWidgets.QTableWidgetItem(casa.m2))
+        self.ui.tb_casas.setItem(row_position, 4, QtWidgets.QTableWidgetItem(casa.nro_dormitorios))
+        self.ui.tb_casas.setItem(row_position, 5, QtWidgets.QTableWidgetItem(casa.nro_banios))
+        self.ui.tb_casas.setItem(row_position, 6, QtWidgets.QTableWidgetItem(casa.amueblado))
+        self.ui.tb_casas.setItem(row_position, 7, QtWidgets.QTableWidgetItem(casa.habitado))
+        self.ui.tb_casas.setItem(row_position, 8, QtWidgets.QTableWidgetItem(casa.precio_ars))
+        self.ui.tb_casas.setItem(row_position, 9, QtWidgets.QTableWidgetItem(casa.precio_uds))
 
+    def agregar_row_departamento(self, departamento):
+        row_position = self.ui.tb_departamentos.rowCount()
+        self.ui.tb_departamentos.insertRow(row_position)
+        self.ui.tb_departamentos.setItem(row_position, 0, QtWidgets.QTableWidgetItem(departamento.direccion))
+        self.ui.tb_departamentos.setItem(row_position, 1, QtWidgets.QTableWidgetItem(departamento.nro_piso))
+        self.ui.tb_departamentos.setItem(row_position, 2, QtWidgets.QTableWidgetItem(departamento.nro_depto))
+        self.ui.tb_departamentos.setItem(row_position, 3, QtWidgets.QTableWidgetItem(departamento.codigo_postal))
+        self.ui.tb_departamentos.setItem(row_position, 4, QtWidgets.QTableWidgetItem(departamento.m2))
+        self.ui.tb_departamentos.setItem(row_position, 5, QtWidgets.QTableWidgetItem(departamento.nro_ambientes))
+        self.ui.tb_departamentos.setItem(row_position, 6, QtWidgets.QTableWidgetItem(departamento.nro_dormitorios))
+        self.ui.tb_departamentos.setItem(row_position, 7, QtWidgets.QTableWidgetItem(departamento.amueblado))
+        self.ui.tb_departamentos.setItem(row_position, 8, QtWidgets.QTableWidgetItem(departamento.habitado))
+        self.ui.tb_departamentos.setItem(row_position, 9, QtWidgets.QTableWidgetItem(departamento.precio_ars))
+        self.ui.tb_departamentos.setItem(row_position, 10, QtWidgets.QTableWidgetItem(departamento.precio_uds))
